e7a71e468019f97073ea73799cd23cc5a67c60ea
A-Chang Lin <ch30529@gmail.com>
2016-07-10 19:23:46 +0800

    Initial Commit

---
+++ Prac4.5Synthesizing Speech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prac4.5Synthesizing Speech
+//
+//  Created by A-Chang Lin on 2016/7/10.
+//  Copyright © 2016年 Sheng-Zhang Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
